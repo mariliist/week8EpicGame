0c8430d281be80488fa2244763e502fbb9c134e6
Mariliis Toon <mariliistoon@gmail.com>
2024-01-08 06:51:03 +0200

    files

---
+++ Hindeline/Program.cs
@@ -0,0 +1,16 @@
+﻿string path = @"C:\Users\JV\data";
+string fileName = "chuck.txt";
+string[] lines = File.ReadAllLines(.Combine(, ));
+
+ShowFileContent();
+
+static ShowFileContent(string[] fileContentArray)
+{
+    int i = ;
+    foreach (line in fileContentArray)
+    {
+        Console.WriteLine($"{i}. {}");
+        i++;
+    }
+}
+
